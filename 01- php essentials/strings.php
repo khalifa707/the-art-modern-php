21be6aedd853f51f0ccdadfcded21f5dd181a541
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-11 15:30:46 +0200

    THE ART OF MODERN PHP

---
+++ 01- php essentials/strings.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="strings.css">
+    <title>Strings in PHP</title>
+</head>
+<body>
+<pre><?php
+    $greeting = "Hello, world!";
+    echo $greeting . "!!!";
+    echo '<br />';
+
+    $name = "John Doe";
+    $subject= "PHP";
+
+    $text = "I am " . $name . " and I am studying " . $subject;
+    echo $text;
+
+    ?></pre>
+</body>
+</html>
+
