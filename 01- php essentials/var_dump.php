997d9c3f0fa669563eb1930014b02cd9b5181a0f
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-11 23:54:50 +0200

    THE ART OF MODERN PHP (Working With Numbers)

---
+++ 01- php essentials/var_dump.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="strings.css">
+    <title>Var Dump</title>
+</head>
+<body>
+
+<pre><?php
+    var_dump('Hello from a var_dump');
+    $message = 'Hello from a var_dump';
+    var_dump($message);
+
+    ?></pre>
+
+</body>
+</html>
