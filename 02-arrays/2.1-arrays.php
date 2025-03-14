b4ecdc14c0ff0a3bc1d12a6a47af337af7a35b4b
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-14 23:15:14 +0200

    THE ART OF MODERN PHP (Working With Arrays)

--- 02-arrays/2.1-arrays.php
+++ 02-arrays/2.1-arrays.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="strings.css">
+    <title>Arrays</title>
+</head>
+<body>
+<pre><?php
+
+    $categories = ['programming', 'business', 'technology', 'entertainment'];
+
+    var_dump($categories);
+    echo $categories[0] ."</br>";
+    echo $categories[1] ."</br>";
+    echo $categories[3] ."</br>";
+    echo $categories[4] ."</br>";
+
+    ?></pre>
+</body>
+</html>
