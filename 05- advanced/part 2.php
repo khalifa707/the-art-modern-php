3891c6e6a64dec41afc7c4dd5904e5017eb18317
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-20 09:50:05 +0200

    Building a project

---
+++ 05- advanced/part 2.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="strings.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<pre><?php
+    if (isset($_GET['price'])) {
+        $price = $_GET['price'];
+        var_dump($price);
+    }
+
+    ?></pre>
+
+</body>
+</html>
